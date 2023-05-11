7342d6fd77c8c8163d743e4d206a4147bd976ed3
Adiityaaaa <89528497+Adiityaaaa@users.noreply.github.com>
2023-05-11 13:59:46 +0530

    Add files via upload

---
+++ history_header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>header</title>
+	    <link rel = "stylesheet" type = "text/css" href = "headerstyle.css">
+</head>
+<body>
+	<div class="header">
+     <img src="nietlogo.png" alt="logo" />
+     <h1>  Noida Institute Of Engineering & Technology  </h1>
+    <div class="header-right">
+     <a class="active" href="history.php">History</a>
+     <a href="https://www.niet.co.in/">About</a>
+  </div>
+</div>
+
+</body>
+</html>
